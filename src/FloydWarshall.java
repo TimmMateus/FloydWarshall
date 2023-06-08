cf486e202874b39e25cfe23f80692fe8d759452b
=Mateus Timm <mateustimm@hotmail.com>
2023-06-07 23:06:14 -0300

    refatoração de metodos

--- src/FloydWarshall.java
+++ src/FloydWarshall.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class FloydWarshall {
     public static void executar(PesosGrafos pesos){
         int tamanho = pesos.getTamnho();
@@ -16,5 +18,33 @@ public class FloydWarshall {
         }
         
         pesos.setPesos(matriz);
+    }
+
+    public static void distancia(PesosGrafos pesos) {
+        int origem, destino;
+        System.out.println("Selecione a letra origem:");
+        origem = receberLetra(pesos);
+        System.out.println("Selecione a letra destino:");
+        destino = receberLetra(pesos);
+        System.out.println("A distancia entre "+(char)(origem+65)+" e "+(char)(destino+65)+" é de "+pesos.distancia(origem, destino));
+    }
+
+    private static int receberLetra(PesosGrafos pesos) {
+        Scanner entrada = new Scanner(System.in);
+        int dado;
+        char in;
+        do{
+            in = entrada.next().charAt(0);
+            dado = converte(in);
+            if(dado<0||dado>pesos.getTamnho()-1){
+                System.out.println("Dado inválido, insira apenas letras correspondentes.");
+            }
+        }while(dado<0||dado>pesos.getTamnho()-1);
+        return dado;
+    }
+
+    private static int converte(char in) {
+        int dado = in;
+        return (dado > 96 ? dado-97 : dado-65);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) throws IOException {
@@ -9,20 +8,6 @@ public class Main{
         System.out.println(pesos.imprimir());
         FloydWarshall.executar(pesos);
         System.out.println(pesos.imprimir());
-        Scanner entrada = new Scanner(System.in);
-        int origem, destino;
-        char in = ' ';
-        do{
-            System.out.print("Selecione a letra origem: ");
-            in = entrada.next().charAt(0);
-            origem= in-65;
-        }while(origem<0||origem>pesos.getTamnho()-1);
-        do{
-            System.out.print("Selecione a letra destino: ");            
-            in = entrada.next().charAt(0);
-            destino = in-65;
-        }while(destino<0||destino>pesos.getTamnho()-1);
-        entrada.close();
-        System.out.println("A distancia entre "+(char)(origem+65)+" e "+(char)(destino+65)+" é de "+pesos.distancia(origem, destino));
+        FloydWarshall.distancia(pesos);
     }
 }
