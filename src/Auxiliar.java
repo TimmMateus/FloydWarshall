4d133628c74dc10e2310f3a427f611efb545de03
Mateus Timm <maretimm@gmail.com>
2023-06-13 19:26:35 -0300

    .

--- src/Auxiliar.java
+++ src/Auxiliar.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Auxiliar {
     public static void esperar(){
         Scanner sc = new Scanner(System.in);
+        System.out.println();
         System.out.println("Clique para continuar");
         sc.nextLine(); 
     }
